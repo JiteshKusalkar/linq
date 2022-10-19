06e58f8143dfc0b2da81fa40ea8b70d9966a8ece
JiteshKusalkar <infinitegod69@gmail.com>
2022-10-19 21:44:34 +0200

    chore(server): set up basic socket connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello world!');
+import cors from "cors";
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_ORIGIN,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to the socket: %s", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected from the socket: %s", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Server started at: %s", PORT);
+});
